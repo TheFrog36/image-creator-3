1bdbd9a79028ec6c3e19db6775e4012306814edf
Frog <artisangiorgio98@gmail.com>
2022-06-20 08:13:56 +0200

    gf

---
+++ rectangle-class.js
@@ -0,0 +1,37 @@
+class Rectangle {
+    
+    constructor(width, height, centerX, centerY, rotation, outerWidth, outerHeight, red, green, blue, alpha){
+        this.width = width
+        this.height = height
+        this.centerX = centerX
+        this.centerY = centerY
+        this.rotation = rotation
+        this.outerWidth = outerWidth
+        this.outerHeight = outerHeight
+        this.red = red
+        this.green = green
+        this.blue = blue
+        this.alpha = alpha
+    }
+
+    fillColors(){
+        this.red = Math.round(Math.random() * 255)
+        this.green = Math.round(Math.random() * 255)
+        this.blue = Math.round(Math.random() * 255)
+        this.alpha = Math.round(Math.random() * 100) / 100
+    }
+
+    generateRBGString(){
+        return `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`
+    }
+    
+    generateCenterCoordinates(canvasWidth, canvasHeight){
+        this.width = Math.random() * canvasWidth
+        this.height = Math.random() * canvasHeight
+    }
+
+    generateOuterRect(){
+        
+    }
+
+}
--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-var folder = "./assets/images";
-let j = 0
-$.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+let width
+let height
+let targetImgColorAvg 
 
-                $("body").append(`<img src="${val}" id = "img${j}">`);
-                const img = document.getElementById('img' + j)
-                img.onload = function() {
-                    console.log(this.width + 'x' + this.height);
-                }
-                j++;
+const targetCanvas = document.getElementById('target-canvas')
+const targetCTX = targetCanvas.getContext('2d')
+const outputCanvas = document.getElementById('output-canvas')
+const outputCTX = outputCanvas.getContext('2d')
+const inputCanvas = document.getElementById('input-canvas')
+const inputCTX = inputCanvas.getContext('2d')
 
-            } 
-        });
+
+function init() {
+    base_image = new Image();
+    base_image.src = './assets/images/aivazovsky.png';
+    base_image.onload = () => {
+        width = base_image.width
+        height = base_image.height
+        setCanvasSizes()  //Imposto la dimensione di tutte le canvas alle dimensioni dell'immagine
+        targetCTX.drawImage(base_image, 0, 0);
+        outputCTX.fillStyle = 'rgba(255, 255, 255, 1)'
+        outputCTX.fillRect(0, 0, width, height)
+        const rect = new Rectangle()
+        
     }
-});
+}
 
-LOL 
+function setCanvasSizes(){
+    targetCanvas.height = height
+    targetCanvas.width = width
+    outputCanvas.height = height
+    outputCanvas.width = width
+    inputCanvas.height = height
+    inputCanvas.width = width
+}
+
+init()
